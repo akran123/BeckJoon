9a472b3a2d1f5820f910b3838a13ca9263479249
akran <nhlk123@naver.com>
2026-03-06 18:22:19 +0900

    feat: 2437 문제 풀이
    그리디 알고리즘 방식
    배열 정렬 후
    현재 index> target 일 시 target 제작 불가

---
+++ Gold/2437.py
@@ -0,0 +1,13 @@
+from itertools import combinations
+
+a=int(input())
+l=list(map(int,input().split(' ')))
+l.sort()
+
+target=1
+for i in l :
+    if target<i :
+        break
+    target+=i
+print(target)
+
